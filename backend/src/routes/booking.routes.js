9a88ecc630ae09b4c3837d6b5986bf732c96247e
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-06-07 11:51:15 -0600

    feat: added zod validation to booking route

--- backend/src/routes/booking.routes.js
+++ backend/src/routes/booking.routes.js
@@ -8,21 +8,25 @@ import {
 } from "../controllers/booking.controller.js";
 import auth from "../middlewares/auth.middleware.js";
 import allowRoles from "../middlewares/role.middleware.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { createBookingSchema, updateBookingStatusSchema, rescheduleBookingSchema } from "../utils/schemas.js";
 
 const router = Router();
 
 router.get("/", auth, getBookings);
-router.post("/", auth, allowRoles("OWNER", "STAFF"), createBooking);
+router.post("/", auth, allowRoles("OWNER", "STAFF"),validate(createBookingSchema), createBooking);
 router.patch(
   "/:id/status",
   auth,
   allowRoles("OWNER", "STAFF"),
+  validate(updateBookingStatusSchema),
   updateBookingStatus,
 );
 router.post(
   "/:id/reschedule",
   auth,
   allowRoles("OWNER", "STAFF"),
+  validate(rescheduleBookingSchema),
   rescheduleBooking,
 );
 router.get("/:id", auth, getBooking);
