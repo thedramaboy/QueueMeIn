8acf417ec83c2f78c01bdcc01be57fa4cc6af833
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-06-07 11:46:51 -0600

    feat: added zod validation to login route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { login, getMe } from "../controllers/auth.controller.js";
 import auth from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { loginSchema } from "../utils/schemas.js";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", validate(loginSchema),login);
 router.get("/me", auth, getMe);
 
 export default router;
