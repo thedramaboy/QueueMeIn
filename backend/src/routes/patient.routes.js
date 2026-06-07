1b5cb04a227a54fe17d39f8e6780578e68d5425a
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-06-07 11:49:08 -0600

    feat: added zod validation to patient route

--- backend/src/routes/patient.routes.js
+++ backend/src/routes/patient.routes.js
@@ -8,13 +8,15 @@ import {
 } from "../controllers/patient.controller.js";
 import auth from "../middlewares/auth.middleware.js";
 import allowRoles from "../middlewares/role.middleware.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { createPatientSchema, updatePatientSchema } from "../utils/schemas.js";
 
 const router = Router();
 
 router.get("/", auth, getPatients);
 router.get("/:id", auth, getPatient);
-router.post("/", auth, allowRoles("OWNER", "STAFF"), createPatient);
-router.put("/:id", auth, allowRoles("OWNER", "STAFF"), updatePatient);
+router.post("/", auth, allowRoles("OWNER", "STAFF"),validate(createPatient), createPatient);
+router.put("/:id", auth, allowRoles("OWNER", "STAFF"),validate(updatePatient), updatePatient);
 router.delete("/:id", auth, allowRoles("OWNER"), deletePatient);
 
 export default router;
