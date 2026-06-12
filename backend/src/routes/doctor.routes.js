1f508de7af1b68691dbb91201320b67b259602eb
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-06-11 20:35:09 -0600

    fix: fix import files from doctor schema

--- backend/src/routes/doctor.routes.js
+++ backend/src/routes/doctor.routes.js
@@ -9,14 +9,14 @@ import {
 import auth from "../middlewares/auth.middleware.js";
 import allowRoles from "../middlewares/role.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { createDoctorSchema } from "../schemas/doctor.schema.js";
+import { createDoctorSchema, updateDoctorSchema } from "../schemas/doctor.schema.js";
 
 const router = Router();
 
 router.get("/", auth, getDoctors);
 router.get("/:id", auth, getDoctor);
 router.post("/", auth, allowRoles("OWNER"),validate(createDoctorSchema), createDoctor);
-router.put("/:id", auth, allowRoles("OWNER"), updateDoctor);
+router.put("/:id", auth, allowRoles("OWNER"), validate(updateDoctorSchema), updateDoctor);
 router.delete("/:id", auth, allowRoles("OWNER"), deleteDoctor);
 
 export default router;
--- backend/src/schemas/doctor.schema.js
+++ backend/src/schemas/doctor.schema.js
@@ -6,3 +6,10 @@ export const createDoctorSchema = z.object({
     phone: z.string().optional().nullable(),
     branchIds: z.array(z.number().int().positive()).min(1, "กรุณาเลือกสาขาอย่างน้อย 1 สาขา")
 })
+
+export const updateDoctorSchema = z.object({
+    name: z.string().min(1).optional(),
+    specialty: z.string().optional().nullable(),
+    phone: z.string().optional().nullable(),
+    branchIds: z.array(z.number().int().positive()).min(1).optional(),
+})
